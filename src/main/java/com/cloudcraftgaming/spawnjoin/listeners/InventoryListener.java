80b3ac2f83a9d2ba493dc7a87f80cad2023cf2e2
NovaFox161 <cloudcraftcontact@gmail.com>
2016-06-15 09:01:39 -0500

    Fix perm check bug for inventory menus

--- src/main/java/com/cloudcraftgaming/spawnjoin/listeners/InventoryListener.java
+++ src/main/java/com/cloudcraftgaming/spawnjoin/listeners/InventoryListener.java
@@ -36,7 +36,7 @@ public class InventoryListener implements Listener {
                     String warpName = meta.getDisplayName();
                     if (LocationChecker.warpExists(warpName)) {
                         player.closeInventory();
-                        if (player.hasPermission("SpawnJoin.warp." + warpName) || player.hasPermission("SpawnJoin.warp.*")) {
+                        if (player.hasPermission("SpawnJoin.use.warp." + warpName) || player.hasPermission("SpawnJoin.use.warp.*")) {
                             DelayChecker.warpDelayCheck(warpName, player);
                         } else {
                             player.sendMessage(MessageManager.getPrefix() + MessageManager.getNoPermMessage());
@@ -50,7 +50,7 @@ public class InventoryListener implements Listener {
                     String hubName = meta.getDisplayName();
                     if (LocationChecker.hubExists(hubName)) {
                         player.closeInventory();
-                        if (player.hasPermission("SpawnJoin.hub." + hubName) || player.hasPermission("SpawnJoin.hub.*")) {
+                        if (player.hasPermission("SpawnJoin.use.hub." + hubName) || player.hasPermission("SpawnJoin.use.hub.*")) {
                             DelayChecker.hubDelayCheck(hubName, player);
                         } else {
                             player.sendMessage(MessageManager.getPrefix() + MessageManager.getNoPermMessage());
@@ -64,7 +64,7 @@ public class InventoryListener implements Listener {
                     String lobbyName = meta.getDisplayName();
                     if (LocationChecker.lobbyExists(lobbyName)) {
                         player.closeInventory();
-                        if (player.hasPermission("SpawnJoin.lobby." + lobbyName) || player.hasPermission("SpawnJoin.lobby.*")) {
+                        if (player.hasPermission("SpawnJoin.use.lobby." + lobbyName) || player.hasPermission("SpawnJoin.use.lobby.*")) {
                             DelayChecker.lobbyDelayCheck(lobbyName, player);
                         } else {
                             player.sendMessage(MessageManager.getPrefix() + MessageManager.getNoPermMessage());
@@ -78,7 +78,7 @@ public class InventoryListener implements Listener {
                     String locName = meta.getDisplayName();
                     if (LocationChecker.spectateExists(locName)) {
                         player.closeInventory();
-                        if (player.hasPermission("SpawnJoin.spectate." + locName) || player.hasPermission("SpawnJoin.spectate.*")) {
+                        if (player.hasPermission("SpawnJoin.use.spectate." + locName) || player.hasPermission("SpawnJoin.use.spectate.*")) {
                             DelayChecker.spectateDelayCheck(locName, player);
                         } else {
                             player.sendMessage(MessageManager.getPrefix() + MessageManager.getNoPermMessage());
